1123dc9b7450e3b16f0a5f26a5a0090f8f1b8061
romanannaev <romanannaev1992@gmail.com>
2020-01-08 16:21:56 +0300

    change secret_key

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'aiqp^ma6f9^d+&voyr0c*re7)6+7(iiru3!0f#zbq%7swh^b_6'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SECURITY_PASSWORD_SALT = 'salt'
     SECURITY_PASSWORD_HASH = 'sha512_crypt'
